4fe7b0970169e56ed127929d4647ac9806acf741
Antonio Gloria <angloria87@gmail.com>
2022-11-01 17:25:59 -0500

    Added ability for player to shoot, added enemyA spawning and shooting

--- game-src.js
+++ game-src.js
@@ -4,6 +4,11 @@ const canvasH = 950;
 
 const ctx = canvas.getContext("2d");
 
+const playerShots = [];
+const enemies = [];
+const enemyShots = []
+
+// Player Images
 const playerF0 = new Image();
 const playerF1 = new Image();
 const playerF2 = new Image();
@@ -18,19 +23,62 @@ playerF3.src = "./img/sprites/player/default/player_03.png";
 playerF4.src = "./img/sprites/player/default/player_04.png";
 playerF5.src = "./img/sprites/player/default/player_05.png";
 
-const playerFrames = [playerF0,playerF1,playerF2,playerF3,playerF4,playerF5]
+const playerFrames = [playerF0, playerF1, playerF2, playerF3, playerF4, playerF5]
 
-class Character {
-  constructor(ctx, sprites, posX, posY) {
+const playerShotImg = new Image();
+playerShotImg.src = "./img/sprites/player/playerShot.png";
+
+// Enemy 1 Images
+const enemyAF0 = new Image();
+const enemyAF1 = new Image();
+const enemyAF2 = new Image();
+const enemyAF3 = new Image();
+const enemyAF4 = new Image();
+const enemyAF5 = new Image();
+
+enemyAF0.src = "./img/sprites/enemyA/default/enemyA_00.png";
+enemyAF1.src = "./img/sprites/enemyA/default/enemyA_01.png";
+enemyAF2.src = "./img/sprites/enemyA/default/enemyA_02.png";
+enemyAF3.src = "./img/sprites/enemyA/default/enemyA_03.png";
+enemyAF4.src = "./img/sprites/enemyA/default/enemyA_04.png";
+enemyAF5.src = "./img/sprites/enemyA/default/enemyA_05.png";
+
+const enemyAFrames = [enemyAF0, enemyAF1, enemyAF2, enemyAF3, enemyAF4, enemyAF5]
+const enemyAShotImg = new Image();
+enemyAShotImg.src = "./img/sprites/enemyA/enemyAShot.png";
+
+// Base Classes
+class Sprite {
+  constructor(ctx, frames) {
     this.ctx = ctx;
-    this.sprites = sprites;
-    this.currentSprite = 0;
-    this.width = this.sprites[0].width;
-    this.height = this.sprites[0].height;
+    this.frames = frames;
+    this.currentFrame = 0;
+    this.width = this.frames[0].width;
+    this.height = this.frames[0].height;
+  }
+
+  draw(x, y) {
+    this.ctx.drawImage(this.frames[this.currentFrame], x, y);
+
+    if (this.frames.length > 1) {
+      this.currentFrame++;
+      if (this.currentFrame >= this.frames.length) {
+        this.currentFrame = 0;
+      }
+    }
+  }
+}
+
+class Entity {
+  constructor(sprite, posX, posY) {
+    this.sprite = sprite;
     this.posX = posX;
     this.posY = posY;
-    this.hp = 100;
-    this.speed = 10;
+    this.speed = 0;
+  }
+
+  setSpeed(speed) {
+    this.speed = speed;
   }
 
   moveRight() {
@@ -49,32 +97,109 @@ class Character {
     this.posY = this.posY + this.speed;
   }
 
-  draw() {
-    this.ctx.drawImage(this.sprites[this.currentSprite], this.posX, this.posY);
-    this.currentSprite++;
-    if (this.currentSprite >= this.sprites.length) {
-      this.currentSprite = 0;
-    }
-  }
-
   getBoundRight() {
-    return this.posX + this.width;
+    return this.posX + this.sprite.width;
   }
 
   getBoundBottom() {
-    return this.posY + this.height;
+    return this.posY + this.sprite.height;
   }
 }
 
-const player = new Character(ctx, playerFrames, canvasW / 2, 700);
+// Game Character classes
+class Player extends Entity {
+  constructor(sprite, posX, posY) {
+    super(sprite, posX, posY);
+    this.hp = 100;
+  }
 
-setInterval(() => {
+  shoot() {
+    let shotSprite = new Sprite(ctx, [playerShotImg])
+    let shotX = this.posX + (this.sprite.width / 2) - (shotSprite.width / 2);
+    let shotY = this.posY - shotSprite.height;
+    let shot = new Entity(shotSprite, shotX, shotY)
+    shot.setSpeed(30);
+    playerShots.push(shot)
+  }
+}
+
+class EnemyA extends Entity {
+  constructor(sprite, posX, posY) {
+    super(sprite, posX, posY);
+    this.hp = 100;
+  }
+
+  shoot() {
+    let shotSprite = new Sprite(ctx, [enemyAShotImg])
+    let shotX = this.posX + (this.sprite.width / 2) - (shotSprite.width / 2);
+    let shotY = this.sprite.height;
+    let shot = new Entity(shotSprite, shotX, shotY)
+    shot.setSpeed(30);
+    enemyShots.push(shot);
+  }
+}
+
+// Enemy functions
+function spawnEnemyA() {
+  let sprite = new Sprite(ctx, enemyAFrames);
+  let enemyX = Math.floor(Math.random() * (canvasW-sprite.width));
+  let enemy = new EnemyA(sprite, enemyX, -sprite.height);
+  enemy.setSpeed(5);
+  enemies.push(enemy)
+  return enemy;
+}
+
+// Player Instances
+const playerSprite = new Sprite(ctx, playerFrames);
+const playerSpawnX = (canvasW - playerSprite.width) / 2;
+const playerSpawnY = playerSprite.height + 650;
+const player = new Player(playerSprite, playerSpawnX, playerSpawnY);
+player.setSpeed(10);
+
+// Interval managing drawing and real-time events
+const gamePlay = setInterval(() => {
   ctx.clearRect(0, 0, canvasW, canvasH)
-  player.draw()
+  player.sprite.draw(player.posX, player.posY);
 
-},1000/60)
+  playerShots.forEach(shot => {
+    shot.sprite.draw(shot.posX, shot.posY);
+    shot.moveUp();
+    if (shot.posY + shot.sprite.height < 0) {
+      playerShots.splice(playerShots.indexOf(shot), 1);
+      shot = null;
+      delete shot;
+    }
+  });
 
-window.addEventListener("keydown", (event) => {
+  let spawnChanceEnemyA = Math.floor(Math.random() * 100)
+  if (spawnChanceEnemyA > 97) {
+    let enemy = spawnEnemyA();
+    enemy.shoot();
+  }
+
+  enemies.forEach(enemy => {
+    enemy.sprite.draw(enemy.posX, enemy.posY);
+    enemy.moveDown();
+    if (enemy.posY > canvasH) {
+      enemies.splice(enemies.indexOf(enemy), 1);
+      enemy = null;
+      delete enemy;
+    }
+  });
+
+  enemyShots.forEach(shot => {
+    shot.sprite.draw(shot.posX, shot.posY);
+    shot.moveDown();
+    if (shot.posY > canvasH) {
+      enemyShots.splice(enemyShots.indexOf(shot), 1);
+      shot = null;
+      delete shot;
+    }
+  });
+},1000/60);
+
+// Player Controls
+window.addEventListener("keydown", (event, canShoot) => {
   switch (event.code) {
     case "ArrowRight":
       if (player.getBoundRight() < canvasW) {
@@ -98,6 +223,10 @@ window.addEventListener("keydown", (event) => {
       if (player.getBoundBottom() < canvasH) {
         player.moveDown();
       }
+      break;
+
+    case "KeyZ":
+      player.shoot();
       break;
   }
 });
